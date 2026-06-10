6c9a46085e8ef6609fa1937977785f9dc73b7a10
diksha jaiswal <https://github.com/dikshaacodes/Summer_Assignment_25114CN190.git>
2026-06-11 00:47:54 +0530

    Day 16 uploaded

---
+++ Day 16/q61.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, sum = 0, total = 0;
+    int arr[100];
+
+    printf("Enter n (numbers from 1 to n): ");
+    scanf("%d", &n);
+
+    printf("Enter %d numbers:\n", n - 1);
+
+    for(i = 0; i < n - 1; i++) {
+        scanf("%d", &arr[i]);
+        sum += arr[i];
+    }
+
+    total = n * (n + 1) / 2;
+
+    printf("Missing Number = %d", total - sum);
+
+    return 0;
+}
---
+++ Day 16/q63.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    int arr[100], n, i, j, sum;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter elements:\n");
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter required sum: ");
+    scanf("%d", &sum);
+
+    for(i = 0; i < n; i++) {
+        for(j = i + 1; j < n; j++) {
+            if(arr[i] + arr[j] == sum) {
+                printf("Pair: %d %d", arr[i], arr[j]);
+                return 0;
+            }
+        }
+    }
+
+    printf("No pair found");
+
+    return 0;
+}
